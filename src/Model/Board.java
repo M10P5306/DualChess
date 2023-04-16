66a202e8a277516fb4549fed0e76270f82232a9d
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2023-04-16 10:50:12 +0200

    Added ruleHandler
    Skapade en ny klass som skapar validmoves arrayer samt granskade algoritmen.

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -10,9 +10,12 @@ public class Board {
 
     private Square[][] squares;
 
+    private RuleHandler ruleHandler;
+
     public Board(Controller controller) {
         this.controller = controller;
         this.squares = new Square[8][8];
+        this.ruleHandler = new RuleHandler(this);
 
         setupSquares();
         setupPieces();
@@ -73,134 +76,17 @@ public class Board {
         ArrayList<Coordinate> validMoves = new ArrayList<>();
 
         if (selectedPiece instanceof Queen || selectedPiece instanceof Bishop || selectedPiece instanceof Rook) {
-            validMoves = specialPieceValidMoves(coordinate);
+            validMoves = ruleHandler.specialPieceValidMoves(coordinate);
             return validMoves;
         }
         else if (selectedPiece instanceof BlackPawn || selectedPiece instanceof WhitePawn) {
-            validMoves = pawnValidMoves(coordinate);
+            validMoves = ruleHandler.pawnValidMoves(coordinate);
             return validMoves;
         }
-
         else {
-
-            validMoves = kingOrKnightValidMoves(coordinate);
-
+            validMoves = ruleHandler.kingOrKnightValidMoves(coordinate);
         }
         return validMoves;
-    }
-
-    public ArrayList<Coordinate> specialPieceValidMoves(Coordinate coordinate) {
-        ArrayList<Coordinate> possibleMoves = getSpecificSquare(coordinate).getPiece().getPossibleMoves();
-        ArrayList<Coordinate> validMoves = new ArrayList<>();
-
-        for (int i = 0; i<possibleMoves.size(); i++) {
-            if (withInRange(coordinate, possibleMoves.get(i))) {
-
-                if (getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)).hasPiece()) {
-
-                    if(!sameColor(getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)), getSpecificSquare(coordinate)))
-                    {
-                        validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
-
-                        if (i % 7 != 0) {
-                            i = i + (7 - ((i+1) % 7));
-                        }
-                        else {
-                            i = i+6;
-                        }
-                    }
-                    else {
-                        if (i % 7 != 0) {
-                            i = i + (7 - ((i+1) % 7));
-                        }
-                        else {
-                            i = i+6;
-                        }
-                    }
-                }
-                else {
-                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
-                }
-            }
-        }
-        return validMoves;
-    }
-
-    public ArrayList<Coordinate> pawnValidMoves(Coordinate coordinate) {
-        ArrayList<Coordinate> possibleMoves = getSpecificSquare(coordinate).getPiece().getPossibleMoves();
-        ArrayList<Coordinate> validMoves = new ArrayList<>();
-
-        if (withInRange(coordinate, possibleMoves.get(0))) {
-            if (!getSpecificSquare(combineCoordinates(possibleMoves.get(0), coordinate)).hasPiece()) {
-                validMoves.add(combineCoordinates(coordinate, possibleMoves.get(0)));
-            }
-        }
-        if (withInRange(coordinate, possibleMoves.get(1))) {
-            if (!getSpecificSquare(combineCoordinates(possibleMoves.get(1), coordinate)).hasPiece() && getSpecificSquare(coordinate).getPiece().getMoves() == 0) {
-                validMoves.add(combineCoordinates(coordinate, possibleMoves.get(1)));
-            }
-        }
-        if (withInRange(coordinate, possibleMoves.get(2))) {
-            if (getSpecificSquare(combineCoordinates(possibleMoves.get(2), coordinate)).hasPiece()) {
-                if (!sameColor(getSpecificSquare(combineCoordinates(possibleMoves.get(2), coordinate)), getSpecificSquare(coordinate)))
-                validMoves.add(combineCoordinates(coordinate, possibleMoves.get(2)));
-            }
-        }
-        if (withInRange(coordinate, possibleMoves.get(3))) {
-            if (getSpecificSquare(combineCoordinates(possibleMoves.get(3), coordinate)).hasPiece()) {
-                if (!sameColor(getSpecificSquare(combineCoordinates(possibleMoves.get(3), coordinate)), getSpecificSquare(coordinate)))
-                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(3)));
-            }
-        }
-        return validMoves;
-    }
-
-    public ArrayList<Coordinate> kingOrKnightValidMoves(Coordinate coordinate) {
-        ArrayList<Coordinate> possibleMoves = getSpecificSquare(coordinate).getPiece().getPossibleMoves();
-        ArrayList<Coordinate> validMoves = new ArrayList<>();
-
-        for (int i = 0; i<possibleMoves.size(); i++) {
-            if (withInRange(coordinate, possibleMoves.get(i))) {
-
-                if (getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)).hasPiece()) {
-
-                    if(!sameColor(getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)), getSpecificSquare(coordinate)))
-                    {
-                        validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
-                    }
-                }
-                else {
-                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
-                }
-            }
-        }
-
-        return validMoves;
-    }
-    public boolean withInRange(Coordinate currentPosition, Coordinate nextMove) {
-
-        int x = currentPosition.getX()+nextMove.getX();
-        int y = currentPosition.getY()+nextMove.getY();
-
-        if (y < 8 && y >= 0 && x < 8 && x >= 0) {
-        return true;
-        }
-        return false;
-    }
-
-    public Coordinate combineCoordinates(Coordinate coordinateOne, Coordinate coordinateTwo) {
-        int x = coordinateOne.getX() + coordinateTwo.getX();
-        int y = coordinateOne.getY() + coordinateTwo.getY();
-
-        return new Coordinate(x, y);
-    }
-
-    public boolean sameColor(Square squareOne, Square squareTwo) {
-
-        if (squareOne.getPiece().getColor().equals(squareTwo.getPiece().getColor())) {
-            return true;
-        }
-        return false;
     }
 
 }
---
+++ src/Model/RuleHandler.java
@@ -0,0 +1,128 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class RuleHandler {
+
+    private Board board;
+
+    public RuleHandler(Board board) {
+        this.board = board;
+    }
+
+    public ArrayList<Coordinate> specialPieceValidMoves(Coordinate coordinate) {
+        ArrayList<Coordinate> possibleMoves = board.getSpecificSquare(coordinate).getPiece().getPossibleMoves();
+        ArrayList<Coordinate> validMoves = new ArrayList<>();
+
+        for (int i = 0; i<possibleMoves.size(); i++) {
+            if (withInRange(coordinate, possibleMoves.get(i))) {
+
+                if (board.getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)).hasPiece()) {
+
+                    if(!sameColor(board.getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)), board.getSpecificSquare(coordinate)))
+                    {
+                        validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
+
+                        if (i % 7 != 0) {
+                            i = i + (7 - ((i+1) % 7));
+                        }
+                        else {
+                            i = i+6;
+                        }
+                    }
+                    else {
+                        if (i % 7 != 0) {
+                            i = i + (7 - ((i+1) % 7));
+                        }
+                        else {
+                            i = i+6;
+                        }
+                    }
+                }
+                else {
+                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
+                }
+            }
+        }
+        return validMoves;
+    }
+
+    public ArrayList<Coordinate> pawnValidMoves(Coordinate coordinate) {
+        ArrayList<Coordinate> possibleMoves = board.getSpecificSquare(coordinate).getPiece().getPossibleMoves();
+        ArrayList<Coordinate> validMoves = new ArrayList<>();
+
+        if (withInRange(coordinate, possibleMoves.get(0))) {
+            if (!board.getSpecificSquare(combineCoordinates(possibleMoves.get(0), coordinate)).hasPiece()) {
+                validMoves.add(combineCoordinates(coordinate, possibleMoves.get(0)));
+            }
+        }
+        if (withInRange(coordinate, possibleMoves.get(1))) {
+            if (!board.getSpecificSquare(combineCoordinates(possibleMoves.get(1), coordinate)).hasPiece() && board.getSpecificSquare(coordinate).getPiece().getMoves() == 0) {
+                validMoves.add(combineCoordinates(coordinate, possibleMoves.get(1)));
+            }
+        }
+        if (withInRange(coordinate, possibleMoves.get(2))) {
+            if (board.getSpecificSquare(combineCoordinates(possibleMoves.get(2), coordinate)).hasPiece()) {
+                if (!sameColor(board.getSpecificSquare(combineCoordinates(possibleMoves.get(2), coordinate)), board.getSpecificSquare(coordinate)))
+                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(2)));
+            }
+        }
+        if (withInRange(coordinate, possibleMoves.get(3))) {
+            if (board.getSpecificSquare(combineCoordinates(possibleMoves.get(3), coordinate)).hasPiece()) {
+                if (!sameColor(board.getSpecificSquare(combineCoordinates(possibleMoves.get(3), coordinate)), board.getSpecificSquare(coordinate)))
+                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(3)));
+            }
+        }
+        return validMoves;
+    }
+
+    public ArrayList<Coordinate> kingOrKnightValidMoves(Coordinate coordinate) {
+        ArrayList<Coordinate> possibleMoves = board.getSpecificSquare(coordinate).getPiece().getPossibleMoves();
+        ArrayList<Coordinate> validMoves = new ArrayList<>();
+
+        for (int i = 0; i<possibleMoves.size(); i++) {
+            if (withInRange(coordinate, possibleMoves.get(i))) {
+
+                if (board.getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)).hasPiece()) {
+
+                    if(!sameColor(board.getSpecificSquare(combineCoordinates(possibleMoves.get(i), coordinate)), board.getSpecificSquare(coordinate)))
+                    {
+                        validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
+                    }
+                }
+                else {
+                    validMoves.add(combineCoordinates(coordinate, possibleMoves.get(i)));
+                }
+            }
+        }
+
+        return validMoves;
+    }
+    public boolean withInRange(Coordinate currentPosition, Coordinate nextMove) {
+
+        int x = currentPosition.getX()+nextMove.getX();
+        int y = currentPosition.getY()+nextMove.getY();
+
+        if (y < 8 && y >= 0 && x < 8 && x >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public Coordinate combineCoordinates(Coordinate coordinateOne, Coordinate coordinateTwo) {
+        int x = coordinateOne.getX() + coordinateTwo.getX();
+        int y = coordinateOne.getY() + coordinateTwo.getY();
+
+        return new Coordinate(x, y);
+    }
+
+    public boolean sameColor(Square squareOne, Square squareTwo) {
+
+        if (squareOne.getPiece().getColor().equals(squareTwo.getPiece().getColor())) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
