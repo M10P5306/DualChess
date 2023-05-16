b6fc617b7b4a5d324656cd7518accf60a5512cea
Adelabokhamis <128479282+Adelabokhamis@users.noreply.github.com>
2023-05-16 16:24:34 +0200

    Javadoc
    Skrev javadoc för AudioPlayer och BoardButton.

--- src/Model/AudioPlayer.java
+++ src/Model/AudioPlayer.java
@@ -5,7 +5,19 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
 
+/**
+ * The Audioplayer class is responsible for playing sound effects
+ *
+ * @author Adel Mohammed Abo khamis.
+ * @author Edin Jahic
+ */
+
 public class AudioPlayer {
+
+    /**
+     * Plays a sound file specified by the file path.
+     * @param filePath the path of the sound file to be played.
+     */
     public void playSound(String filePath) {
         Clip clip = null;
 
--- src/View/BoardButton.java
+++ src/View/BoardButton.java
@@ -3,9 +3,22 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The BoardButton class represents a customized button used in the board of the chess game.
+ * @author Adel Mohammed Abo Khamis.
+ * @author Hugo Andersson.
+ * @author Edin Jahic.
+ * @author Mikael Nilsson.
+ */
 public class BoardButton extends JButton {
 
     private Color defaultColor;
+
+    /**
+     * Constructs a BoardButton with the specified coordinates on the chess board.
+     * @param x the x-coordinate of the button on the chess board.
+     * @param y the y-coordinate of the button on the chess board.
+     */
 
     public BoardButton(int x, int y) {
         if ((x + y) % 2 == 0) {
@@ -19,6 +32,9 @@ public class BoardButton extends JButton {
         this.setBorderPainted(false);
     }
 
+    /**
+     * Restores the default background color of the button.
+     */
     public void restoreDefaultColor() {
         this.setBackground(defaultColor);
     }
