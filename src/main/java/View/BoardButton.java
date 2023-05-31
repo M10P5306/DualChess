5d8d5d964cafdc6739ffa2d18a1c0541c4e96500
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2023-05-31 11:51:57 +0200

    Update BoardButton.java
    Bytte färg

--- src/main/java/View/BoardButton.java
+++ src/main/java/View/BoardButton.java
@@ -8,7 +8,7 @@ public class BoardButton extends JButton {
     private Color defaultColor;
 
     public BoardButton(int x, int y) {
-        if ((x + y) % 2 == 0) {
+        if ((x + y) % 2 != 0) {
             defaultColor = new Color(255, 255, 255);
         } else {
             defaultColor = new Color(100, 100, 100);
