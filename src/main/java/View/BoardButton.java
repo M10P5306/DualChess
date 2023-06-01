0965a3edadcf25b28385f446d0be4c30f37661a5
M10P5306 <Jag_har_klippt_mig@hotmail.com>
2023-06-01 09:15:40 +0200

    Update BoardButton.java
    Javadoc

--- src/main/java/View/BoardButton.java
+++ src/main/java/View/BoardButton.java
@@ -3,10 +3,24 @@ package View;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * This class is the graphical representation of a square on the board. By interacting with this button the Square class in model is modified.
+ * @see Model.Square
+ * @author Hugo Andersson, Edin Jahic, Adel Mohammed Abo Khamis and Mikael Nilsson.
+ */
+
 public class BoardButton extends JButton {
 
+    /**
+     * Color of the button used to reset it after a move has been made or a piece has been deselected.
+     */
     private Color defaultColor;
 
+    /**
+     * Constructor where the default color of the button is decided.
+     * @param x position of the button.
+     * @param y position of the button.
+     */
     public BoardButton(int x, int y) {
         if ((x + y) % 2 == 0) {
             defaultColor = new Color(100, 100, 100);
@@ -20,6 +34,9 @@ public class BoardButton extends JButton {
         this.setBorderPainted(false);
     }
 
+    /**
+     * method for resetting the color of the button.
+     */
     public void restoreDefaultColor() {
         this.setBackground(defaultColor);
     }
